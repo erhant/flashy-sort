041b8df51ea8c272eefcd18325cf1f4cd3c2afb5
Erhan Tezcan <erhant96@hotmail.com>
2021-03-29 16:28:01 +0300

    bug bug buggy bug!

--- include/flashier.h
+++ include/flashier.h
@@ -11,7 +11,7 @@
 
 // This is the initial code from years ago (2016-ish)! It has A LOT OF redundant code in it.
 template <typename T>
-void flashier_sort(T* arr, int n) { 
+void flashier_sort_old(T* arr, int n) { 
   // Variables
   int pos, i, j, offset, maxPlacer;
   int *collisions, *offsets, *newCollisions;
@@ -102,4 +102,86 @@ void flashier_sort(T* arr, int n) {
   free(offsets); 
 }
 
- 
+ /**
+  * @brief Sort an array of numeric values using Flashier Sort.
+  * 
+  * Comparing to the old code:  
+  * Denominator of pos is memoized.
+  * Does not use newArr, newCollisions or offsets. Uses an extra placements array (TODO optimize)
+  * 
+  * @tparam T: type of the array
+  * @param arr: array pointer
+  * @param n: size of the array 
+  */
+template <typename T>
+void flashier_sort(T* arr, int n) { 
+  // Variables
+  int pos, i, j, offset, maxctr = 1;
+  int *collisions, *placements, c_i, p_i; 
+  T arrmin, arrmax, tmp;
+
+  // 1 - Find min and max, place max values at the end [ O(n) time, O(1) space ]
+  arrmin = arr[0];
+  arrmax = arr[n-1];
+  for (i = 1; i < n-1; ++i) {
+    if (arr[i] > arrmax) { // new max found
+      arrmax = arr[i]; 
+      tmp = arr[n-1]; arr[n-1] = arr[i]; arr[i] = tmp;
+      maxctr=1;
+    } else if (arr[i] == arrmax) { // another equal max found
+      tmp = arr[n-maxctr-1]; arr[n-maxctr-1] = arr[i]; arr[i] = tmp;      
+      maxctr++;
+    }
+
+    if (arr[i] < arrmin) arrmin = arr[i]; 
+    
+  }
+  if (arrmin == arrmax) return; // max=min means sorted already
+
+  const double pos_const = double(arrmax-arrmin) / double(n-1);
+  // 2 - Calculate collisions [ O(n) time O(n) space ]
+  collisions = (int *)calloc(n - maxctr, sizeof(int)); // initially zeros
+  for (i = 0; i < n - maxctr; ++i) 
+    collisions[int(double(arr[i]-arrmin)/double(arrmax-arrmin)*double(n-1))]++;
+
+  // 3 - Calculate offsets [ O(n) time O(1) space ] 
+  offset = 0;
+  for (i = 0; i < n - maxctr; ++i) {
+    c_i = collisions[i];
+    if (c_i==0) offset--; // there is an empty slot, decrease offset
+    collisions[i] += offset;
+    if (c_i>1) offset += c_i-1; // there are collisions, increase offset
+  } 
+
+  // 4 - Calculate placements [ O(n) time O(n) space ]
+  placements = (int *)malloc((n - maxctr) * sizeof(int)); // initially zeros
+  for (i = 0; i < n - maxctr; ++i) {
+    // Calculate position
+    pos = int(double(arr[i]-arrmin)/double(arrmax-arrmin)*double(n-1));
+    placements[i] = pos + collisions[pos] - 1;
+    collisions[pos]--;
+  } 
+
+  // 5 - In-line placing [ O(n) time O(1) space ]
+  for (i = 0; i<n - maxctr; ++i) { 
+    p_i = placements[i];
+    // Check index if it has already been swapped before
+    while (p_i < i) p_i = placements[p_i];
+    // Swap the position of elements
+    tmp = arr[i]; arr[i] = arr[p_i]; arr[p_i] = tmp;
+  } 
+
+  // 6 - Insertion Sort [ O(n^2) time worst, but is working on a nearly sorted array ]
+  for (i = 1; i<n - maxctr; ++i) { 
+    tmp = arr[i];
+    j = i-1;
+    while (j >= 0 && tmp < arr[j]) {
+      arr[j + 1] = arr[j];
+      j -= 1;
+    }
+    arr[j + 1] = tmp;
+  }
+   
+  free(collisions);
+  free(placements); 
+}
--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,8 @@
 
 
 template<typename T>
-void test_array(int N, bool print = false) { 
-  T* init_arr = create_array<T>(N, 1.0, 6.0);
+void test_array(int N, T min, T max, bool print = false) { 
+  T* init_arr = create_array<T>(N, min, max);
   T* arr = (T *)malloc(N * sizeof(T));  
     
   std::memcpy(arr, init_arr, N * sizeof(T));
@@ -28,7 +28,7 @@ void test_array(int N, bool print = false) {
   if (print) { 
     print_array(arr, N, "Before: ");  
   }
-  flashier_sort_optim<T>(arr, N);
+  flashier_sort<T>(arr, N);
   if (print) {  
     print_array(arr, N, "\nAfter Flashier Sort: "); 
   }
@@ -38,17 +38,36 @@ void test_array(int N, bool print = false) {
   free(init_arr);
 }
 
+typedef enum {
+  INTEGER = 0,
+  DOUBLE,
+  FLOAT
+} datatype_e;
+
 int main(int argc, char *argv[]) {
   int N = 50;
   bool print = false;
+  double min = 1.0, max=10.0;
+  datatype_e type = INTEGER;
   for(int ac = 1; ac < argc; ac++) {
     if      (MATCH_INPUT("-n"))       { N = atoi(argv[++ac]); }     // size of array
     else if (MATCH_INPUT("-p"))       { print = true; }             // print arrays
-    else    {printf("Usage: %s -n <size>\n", argv[0]); return -1;}
+    else if (MATCH_INPUT("--min"))    { min = atof(argv[++ac]); }   // min value
+    else if (MATCH_INPUT("--max"))    { max = atof(argv[++ac]); }   // max value
+    else if (MATCH_INPUT("--int"))    { type = INTEGER; }           // int array
+    else if (MATCH_INPUT("--float"))  { type = FLOAT; }             // float array
+    else if (MATCH_INPUT("--double")) { type = DOUBLE; }            // double array
+    else    {printf("Usage: %s -n <size> -p --min <num> --max <num> --int --float --double\n", argv[0]); return -1;}
   }
-  printf("Running with N: %d\n", N);
-  assert(N >= 10); // TODO: below 10 is bug?
-  test_array<float>(N, print);
+  printf("Running with N: %d\n", N); 
+  if (type == INTEGER) {
+    test_array<int>(N, min, max, print);
+  } else if (type == FLOAT) {
+    test_array<float>(N, min, max, print);
+  } else if (type == DOUBLE) {
+    test_array<double>(N, min, max, print);
+  }
+  
 
   return 0;
 }
